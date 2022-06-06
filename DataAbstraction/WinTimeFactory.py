8f649a305c0a9053038c55eaeb7938559dfd4e01
Daniel Tebart <daniel.tebart@rwth-aachen.de>
2022-06-06 20:49:37 +0200

    Win time is now scraped from an existing race card.

--- DataAbstraction/WinTimeFactory.py
+++ DataAbstraction/WinTimeFactory.py
@@ -2,33 +2,47 @@ from DataAbstraction.RaceCard import RaceCard
 from DataCollection.Scraper import get_scraper
 from bs4 import BeautifulSoup
 
+from Persistence.RaceCardPersistence import RaceCardsPersistence
+
 
 class WinTimeFactory:
 
     def __init__(self):
         self.__scraper = get_scraper()
+        self.__base_results_url = "https://t.attheraces.com/results"
 
     def get_win_time(self, race_card: RaceCard) -> float:
-        result_doc = self.__scraper.request_html("https://t.attheraces.com/results/05-Jun-2022")
+        results_url = f"{self.__base_results_url}/{race_card.datetime.date()}"
+        result_doc = self.__scraper.request_html(results_url)
+        article = self.__find_article_of_race_card(result_doc, race_card)
+        win_time_text = self.__find_win_time_text(article)
+
+        return self.__win_time_text_to_seconds(win_time_text)
+
+    def __find_article_of_race_card(self, result_doc: str, race_card: RaceCard):
         soup = BeautifulSoup(result_doc, 'html.parser')
 
-        #for b_elem in soup.find_all('b'):
-        #    print(b_elem)
+        for link in soup.find_all('a', href=True):
+            if self.__link_corresponds_to_race_card(link, race_card):
+                return link.findParent("article")
 
-        for a in soup.find_all('a', href=True):
-            if "/racecard/Goodwood" in a['href'] and "button" not in a["class"]:
-                race_card_number = a.findPrevious().text
-                if int(race_card_number) == 2:
-                    article = a.findParent("article")
-                    win_time_div = [div for div in article.find_all("div") if "Win Time" in div.next_element.text][0]
-                    win_time_start_idx = self.__find_win_time_start_idx(win_time_div.text)
-                    win_time_end_idx = win_time_div.text.find("s") + 1
-                    print(win_time_div)
-                    win_time_text = win_time_div.text[win_time_start_idx:win_time_end_idx]
-                    print(win_time_text)
-                    return self.__win_time_text_to_seconds(win_time_text)
+    def __find_win_time_text(self, article) -> str:
+        win_time_div = [div for div in article.find_all("div") if "Win Time" in div.next_element.text][0]
+        win_time_start_idx = self.__find_win_time_start_idx(win_time_div.text)
+        win_time_end_idx = win_time_div.text.find("s") + 1
 
-        return -1
+        return win_time_div.text[win_time_start_idx:win_time_end_idx]
+
+    def __link_corresponds_to_race_card(self, link, race_card: RaceCard) -> bool:
+        if f"/racecard/{race_card.title}" not in link['href']:
+            return False
+
+        if "button" in link["class"]:
+            return False
+
+        link_number = int(link.findPrevious().text)
+
+        return link_number == race_card.number
 
     def __find_win_time_start_idx(self, div_text: str) -> int:
         for i, c in enumerate(div_text):
@@ -53,7 +67,11 @@ class WinTimeFactory:
 
 def main():
     win_time_factory = WinTimeFactory()
-    win_time = win_time_factory.get_win_time(None)
+
+    raw_races = RaceCardsPersistence("test_race_cards").load_raw()
+    race_cards = [RaceCard(race_id, raw_races[race_id], remove_non_starters=False) for race_id in raw_races]
+
+    win_time = win_time_factory.get_win_time(race_cards[1])
     print(win_time)
 
 
