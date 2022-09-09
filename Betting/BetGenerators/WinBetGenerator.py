274241b281fef321c6007f8e99289fad44275c1b
daniel <daniel.tebart@googlemail.com>
2022-09-09 03:24:14 +0200

    Replaced slow dataframe betting generation by faster numpy calculations

--- Betting/BetGenerators/WinBetGenerator.py
+++ Betting/BetGenerators/WinBetGenerator.py
@@ -16,32 +16,26 @@ class WinBetGenerator(BetGenerator):
     def add_bets(self, race_cards_sample: RaceCardsSample, betting_slips: Dict[str, BettingSlip]) -> None:
         sample_df = race_cards_sample.race_cards_dataframe
 
-        odds = sample_df.loc[:, Horse.CURRENT_ODDS_KEY]
-        win_probabilities = sample_df.loc[:, "win_probability"]
+        horse_ids = sample_df.loc[:, Horse.HORSE_ID_KEY].values
+        odds = sample_df.loc[:, Horse.CURRENT_ODDS_KEY].values
+        win_probabilities = sample_df.loc[:, "win_probability"].values
+        win_indicators = sample_df.loc[:, Horse.HAS_WON_KEY].values
 
-        sample_df.loc[:, "expected_value"] = odds * win_probabilities
+        expected_values = odds * win_probabilities
 
-        sample_df = sample_df[sample_df["expected_value"] > (1.0 + self.additional_ev_threshold)]
+        for i in range(len(horse_ids)):
+            if expected_values[i] > (1.0 + self.additional_ev_threshold):
+                numerator = expected_values[i] - 1
+                denominator = odds[i] - 1
+                stakes_fraction = numerator / denominator
 
-        kelly_numerator = sample_df.loc[:, "expected_value"] - 1
-        kelly_denominator = sample_df.loc[:, Horse.CURRENT_ODDS_KEY] - 1
-        sample_df["stakes_fraction"] = kelly_numerator / kelly_denominator
+                stakes = stakes_fraction * self.bet_limit
 
-        sample_df["stakes"] = sample_df["stakes_fraction"] * self.bet_limit
-        sample_df.loc[sample_df["stakes"] < 0.5, "stakes"] = 0.0
+                if stakes >= 0.5:
+                    new_bet = Bet(horse_ids[i], odds[i], stakes, stakes_fraction)
 
-        for index, row in sample_df.iterrows():
-            horse_id = str(int(row[Horse.HORSE_ID_KEY]))
-            odds = float(row[Horse.CURRENT_ODDS_KEY])
-            stakes = float(row["stakes"])
-            stakes_fraction = float(row["stakes_fraction"])
-            has_won = int(row[Horse.HAS_WON_KEY])
-            new_bet = Bet(horse_id, odds, stakes, stakes_fraction)
+                    betting_slip = betting_slips[str(race_cards_sample.race_keys[i])]
+                    betting_slip.add_bet(new_bet)
 
-            date_time = row[RaceCard.DATETIME_KEY]
-
-            betting_slip = betting_slips[str(date_time)]
-            betting_slip.add_bet(new_bet)
-
-            if has_won:
-                betting_slip.winner_id = horse_id
+                    if win_indicators[i]:
+                        betting_slip.winner_id = horse_ids[i]
