123d0c52332b4380a1cd11fb8f5d985096990dc8
daniel <daniel.tebart@googlemail.com>
2023-02-23 19:56:31 +0100

    Shifted common probabilizing calculations into the base probabilizer class.

--- Model/Probabilizing/Probabilizer.py
+++ Model/Probabilizing/Probabilizer.py
@@ -1,14 +1,17 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 
+import numpy as np
+import pandas as pd
 from numpy import ndarray
 
 from DataAbstraction.Present.HorseResult import HorseResult
+from DataAbstraction.Present.RaceCard import RaceCard
 from Model.Betting.Bets.Bet import Bet
 from Model.Estimation.RaceEventProbabilities import RaceEventProbabilities
 from SampleExtraction.RaceCardsSample import RaceCardsSample
 
 
-class Probabilizer:
+class Probabilizer(ABC):
 
     def __init__(self):
         pass
@@ -16,6 +19,18 @@ class Probabilizer:
     @abstractmethod
     def create_event_probabilities(self, race_cards_sample: RaceCardsSample, scores: ndarray) -> RaceEventProbabilities:
         pass
+
+    def set_win_probabilities(self, race_cards_dataframe: pd.DataFrame, scores: ndarray) -> pd.DataFrame:
+        race_cards_dataframe.loc[:, "score"] = scores
+
+        race_cards_dataframe.loc[:, "exp_score"] = np.exp(race_cards_dataframe.loc[:, "score"])
+        score_sums = race_cards_dataframe.groupby([RaceCard.RACE_ID_KEY]).agg(sum_exp_scores=("exp_score", "sum"))
+        race_cards_dataframe = race_cards_dataframe.merge(right=score_sums, on=RaceCard.RACE_ID_KEY, how="inner")
+
+        race_cards_dataframe.loc[:, "win_probability"] = \
+            race_cards_dataframe.loc[:, "exp_score"] / race_cards_dataframe.loc[:, "sum_exp_scores"]
+
+        return race_cards_dataframe
 
     @abstractmethod
     def create_bet(self, horse_result: HorseResult, stakes_fraction: float) -> Bet:
--- Model/Probabilizing/WinProbabilizer.py
+++ Model/Probabilizing/WinProbabilizer.py
@@ -19,9 +19,7 @@ class WinProbabilizer(Probabilizer):
 
     def create_event_probabilities(self, race_cards_sample: RaceCardsSample, scores: ndarray) -> RaceEventProbabilities:
         race_cards_dataframe = race_cards_sample.race_cards_dataframe
-        race_cards_dataframe.loc[:, "score"] = scores
-
-        race_cards_dataframe = self.set_win_probabilities(race_cards_dataframe)
+        race_cards_dataframe = self.set_win_probabilities(race_cards_dataframe, scores)
         race_cards_dataframe["place_probability"] = "0"
 
         race_cards_dataframe["expected_value"] = race_cards_dataframe[Horse.WIN_PROBABILITY_KEY] \
@@ -29,16 +27,6 @@ class WinProbabilizer(Probabilizer):
                                                  * (1 - Bet.WIN_COMMISION) - (1 + Bet.BET_TAX)
 
         return RaceEventProbabilities(race_cards_dataframe)
-
-    def set_win_probabilities(self, race_cards_dataframe: pd.DataFrame) -> pd.DataFrame:
-        race_cards_dataframe.loc[:, "exp_score"] = np.exp(race_cards_dataframe.loc[:, "score"])
-        score_sums = race_cards_dataframe.groupby([RaceCard.RACE_ID_KEY]).agg(sum_exp_scores=("exp_score", "sum"))
-        race_cards_dataframe = race_cards_dataframe.merge(right=score_sums, on=RaceCard.RACE_ID_KEY, how="inner")
-
-        race_cards_dataframe.loc[:, "win_probability"] = \
-            race_cards_dataframe.loc[:, "exp_score"] / race_cards_dataframe.loc[:, "sum_exp_scores"]
-
-        return race_cards_dataframe
 
     def create_bet(self, horse_result: HorseResult, stakes_fraction: float) -> Bet:
         return WinBet([horse_result], stakes_fraction)
